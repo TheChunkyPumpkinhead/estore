14112c913dc5181ca6caa9c1b81ba98c75383be7
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2023-11-16 13:46:49 -0800

    Updated the Logo

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {/* <div className="header bg-dark "> */}
-      <div className="row">
-        <div className="brand my-1">
-          <div>
-            <h1> eStore</h1>
-            <div className="inp-container p-0 my-4 w-50 h-25 bg-pink">
-              <div className="form-control">
-                <button>
-                  <i className="fa fa-search" />
-                </button>
+      <div className="header bg-dark ">
+        <div className="row">
+          <div className="brand my-1">
+            <div>
+              <h1> eStore</h1>
+              <div className="inp-container p-0 my-4 w-50 h-25 bg-pink">
+                <div className="form-control">
+                  <button>
+                    <i className="fa fa-search" />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* </div> */}
     </div>
   );
 }
