78d3913cc9ed497835379d53da8fd026e716fae4
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2023-11-16 13:38:55 -0800

    Created  an input box and header.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="header ">
-        <div className="row">
-          <div className="brand my-1">
-            <div>
-              <h1> eStore</h1>
+      {/* <div className="header bg-dark "> */}
+      <div className="row">
+        <div className="brand my-1">
+          <div>
+            <h1> eStore</h1>
+            <div className="inp-container p-0 my-4 w-50 h-25 bg-pink">
+              <div className="form-control">
+                <button>
+                  <i className="fa fa-search" />
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      {/* </div> */}
     </div>
   );
 }
