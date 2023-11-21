f54b61e5413f3622aec742c405fcba4b6e7ee5ee
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2023-11-20 17:32:19 -0800

    Created Category Navigation Bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import TopNav from "./Components/TopNav";
+import CatNav from "./Components/CatNav";
 
 function App() {
   return (
     <div className="App">
       <TopNav />
+      <CatNav />
     </div>
   );
 }
