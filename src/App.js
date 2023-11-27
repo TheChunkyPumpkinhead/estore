bafb59294724d3ccf5611ca4511577673aeec465
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2023-11-27 12:09:25 -0800

    Created Component: MainComponent

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import TopNav from "./Components/TopNav";
 import CatNav from "./Components/CatNav";
+import MainComponent from "./Components/MainComponent";
 
 function App() {
   return (
     <div className="App">
       <TopNav />
       <CatNav />
+      <MainComponent />
     </div>
   );
 }
---
+++ src/Components/MainComponent/index.js
@@ -0,0 +1,10 @@
+import "./_main-component.scss";
+
+const MainComponent = () => {
+  return (
+    <div>
+      <h1> MainComponent</h1>
+    </div>
+  );
+};
+export default MainComponent;
