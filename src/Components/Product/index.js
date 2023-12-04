0868bebc78e8a8064018316dced84d818dc36fd3
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2023-12-04 13:30:47 -0800

    styled the product card fully

--- src/Components/Product/index.js
+++ src/Components/Product/index.js
@@ -4,14 +4,14 @@ const Products = () => {
   return (
     <div>
       <div className="mx-5 p-3 col-lg-3 col-md-6 product-card">
-        <div>
+        <div className="product-image-container">
           <img src={require("../../assets/images/shop/shop-1.jpg")} />
         </div>
-        <div>
+        <div className="product-info">
           <h5>
             <a href="#">Jacket</a>
           </h5>
-          <p> $45 </p>
+          <p className="product-price"> $45 </p>
         </div>
       </div>
     </div>
